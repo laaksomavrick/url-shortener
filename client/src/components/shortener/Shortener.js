aca13cc5f9eb9abc25fa1d8911d446422ecdd5a9
mlaakso@dev <laakso.mavrick@gmail.com>
2017-09-15 23:14:24 -0400

    Posting + getting short url from client

--- client/src/components/shortener/Shortener.js
+++ client/src/components/shortener/Shortener.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap'
-import { isUrl } from '../../services/UrlService'
+import { isUrl, postUrl } from '../../services/UrlService'
 
 class Shortener extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            url: ''
+            url: '',
+            key: null,            
+            loading: false,
+            error: false
         }
         this.getValidationState = this.getValidationState.bind(this) 
         this.handleChange = this.handleChange.bind(this)           
@@ -27,7 +30,7 @@ class Shortener extends Component {
     }
 
     handleKeyPress(e) {
-        if(e.charCode==13){
+        if(e.charCode === 13){
             e.preventDefault()            
                 this.submit()   
         }
@@ -36,15 +39,31 @@ class Shortener extends Component {
     submit() {
         if (this.getValidationState() !== 'success') { return }
 
-        //todo post
+        postUrl('http://localhost:3001', this.state.url)
+            .then((json) => {
+                if (json.key) {
+                    this.setState({key: json.key})                    
+                } else {
+                    this.setState({error: true})
+                }
+            })
 
-        console.log(this.state.url)
     }
 
     render() {
+
+        const ShortenedUrl = (props) => {
+            const shortenedUrl = props.encodedUrl
+            if (shortenedUrl) {
+                return <span>Short Url: http://localhost:3000/{shortenedUrl}</span>
+            } else {
+                return null;
+            }
+        }
+
         return (
             <div className="row">
-                <h1>Gem Url</h1>
+                <h1>Long Url</h1>
                 
                 <form>
                     <FormGroup 
@@ -69,6 +88,7 @@ class Shortener extends Component {
                     </FormGroup>
                 </form>
 
+                <ShortenedUrl encodedUrl={this.state.key}/>
 
             </div>
         )
--- client/src/services/UrlService.js
+++ client/src/services/UrlService.js
@@ -1,3 +1,29 @@
 export const isUrl = (str) => {
     return str.match(/^[a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)$/)
 }
+
+export const postUrl = (url, data) => {
+
+    const body = {url: data}
+
+    return post(url, body)
+        .then(
+            response => response.json(),
+            error => null
+        )
+        
+}
+
+
+const post = (url, data) => {
+
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      })
+
+}
